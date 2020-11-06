703449116dcd43514cc2689c7e591f033535935a
CiTX <SladostniyAmurchik@gmail.com>
2020-11-06 16:58:41 +0400

    Update script-load_2.8.js

--- js/script-load_2.8.js
+++ js/script-load_2.8.js
@@ -1782,11 +1782,12 @@ window.cytubeEnhanced.addModule('favouritePictures', function (app) {
 	             '<span class="input-group-btn">' +
 	                '<label for="import-pictures" class="btn btn-sm btn-default" style="border-radius:0;">' + app.t('favPics[.]Import pictures') + '</label>' +
 	                '<input type="file" style="display:none;" id="import-pictures" name="pictures-import">' +
-	            '</span></br>' +
+	            '</span>' +
+		    '</br><div>'+
 	            '<input type="text" id="picture-address" class="form-control input-sm" placeholder="' + app.t('favPics[.]Picture url') + '">' +
 	            '<span class="input-group-btn">' +
 	                '<button id="add-picture-btn" class="btn btn-sm btn-default" style="border-radius:0;" type="button">' + app.t('favPics[.]Add') + '</button>' +
-	            '</span>' +
+	            '</span>' +'</div>'+
 	        '</div>')
 	        .appendTo(this.$favouritePicturesControlPanel);
 
